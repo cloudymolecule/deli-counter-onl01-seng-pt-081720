50a7f29008c7c3f11d7ab29bbf054e52cb33f8d3
Dario Carlino <baires82@gmail.com>
2020-08-21 02:21:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(place)
+  if place.length == 0
+    puts "The line is currently empty."
+  else
+    list
+    position = 1
+    place.each do |name|
+      position +=
+      list << "#{position} #{name} "
+    end
+    list.join(" ")
+    puts "The line is currently: #{list}"
+  end
+end
